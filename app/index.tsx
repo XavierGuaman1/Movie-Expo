fdddf731a2e923e4057805beb9231f9a0f6118e8
XavierGuaman1 <116098951+XavierGuaman1@users.noreply.github.com>
2024-02-02 08:47:28 -0500

    Trabajo en clase viernes 2/2/24

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,5 @@
-import { Github, Twitter } from "@tamagui/lucide-icons";
 import { Link, useRouter } from "expo-router";
-import {
-  Button,
-  H1,
-  ListItem,
-  Image,
-  Paragraph,
-  Separator,
-  YGroup,
-  YStack
-} from "tamagui";
+import { Button, H1, Image, Paragraph, Separator, YGroup, YStack } from "tamagui";
 
 import { MySafeAreaView } from "../components/MySafeAreaView";
 import { MyStack } from "../components/MyStack";
@@ -23,77 +13,50 @@ export default function Home() {
         <YStack
           space="$4"
           maxWidth={600}
+          flex={1}
+          justifyContent="space-between"
+          alignItems="center"
         >
-          <H1 textAlign="center">Welcome to Tamagui.</H1>
-          <Paragraph textAlign="center">
-            Here&apos;s a basic starter to show navigating from one screen to
-            another.
-          </Paragraph>
-        </YStack>
-        <Image
-          source={{
-            uri: require("../assets/IndianaJones.jpeg"),
-            width:250,
-            height:250
-          }}
-          />
+          <YStack space="$2">
+            <Image
+              source={{
+                uri: require("../assets/IndianaJones.jpeg"),
+                width: 350,
+                height: 450,
+              }}
+              resizeMode="cover"
+              borderRadius={10}
+            />
+          </YStack>
 
-        <YStack space="$2.5">
-          <Button onPress={() => router.push("/users/testuser")}>
-            Go to user page
-          </Button>
-          <Button onPress={() => router.push("/tabs")}>Go to tabs page</Button>
-        </YStack>
+          <YStack>
+            <H1 textAlign="center">Indiana Jones.</H1>
+            <Paragraph textAlign="center">
+              Indiana Jones is a fictional character created by filmmaker George Lucas and starred by actor Harrison Ford in the famous adventure film saga.
+            </Paragraph>
+          </YStack>
 
-        <YStack space="$5">
-          <YGroup
-            bordered
-            separator={<Separator />}
-            theme="green"
-          >
-            <YGroup.Item>
-              <Link
-                asChild
-                href="https://twitter.com/natebirdman"
-                target="_blank"
-              >
-                <ListItem
-                  hoverTheme
-                  title="Nate"
-                  pressTheme
-                  icon={Twitter}
-                />
-              </Link>
-            </YGroup.Item>
-            <YGroup.Item>
-              <Link
-                asChild
-                href="https://github.com/tamagui/tamagui"
-                target="_blank"
-              >
-                <ListItem
-                  hoverTheme
-                  pressTheme
-                  title="Tamagui"
-                  icon={Github}
-                />
-              </Link>
-            </YGroup.Item>
-            <YGroup.Item>
-              <Link
-                asChild
-                href="https://github.com/ivopr/tamagui-expo"
-                target="_blank"
-              >
-                <ListItem
-                  hoverTheme
-                  pressTheme
-                  title="This Template"
-                  icon={Github}
-                />
-              </Link>
-            </YGroup.Item>
-          </YGroup>
+          <YStack space="$2.5">
+          <Button
+  onPress={() => router.push("/tabs")}
+  style={{
+    backgroundColor: "#FF9700", 
+    padding: 10, 
+    borderRadius: 5, 
+    borderWidth: 1, 
+    borderColor: "#FEFEFE", 
+    width: 200,
+   
+  }}
+  textStyle={{
+    color: "red", 
+    fontWeight: "bold", 
+  }}
+>
+  Begin
+</Button>
+
+          </YStack>
         </YStack>
       </MyStack>
     </MySafeAreaView>
